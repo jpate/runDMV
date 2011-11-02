b3dc44365a362a0c0f694b1be3a55144dfdabd70
John K Pate <j.k.pate@sms.ed.ac.uk>
2011-11-02 13:21:30 +0000

    setClasspath for environments w/o scala

--- local/bin/setClasspath.sh
+++ local/bin/setClasspath.sh
@@ -2,5 +2,5 @@
 
 LIBROOT=$1
 
-export CLASSPATH=$LIBROOT/jopt-simple-4.0-beta1.jar:$LIBROOT/predictabilityparsing_2.9.1-0.01.jar:$LIBROOT/rundmv_2.9.1-0.1.jar:$LIBROOT/akka-actor-1.1.3.jar
+export CLASSPATH=$LIBROOT/jopt-simple-4.0-beta1.jar:$LIBROOT/predictabilityparsing_2.9.1-0.01.jar:$LIBROOT/rundmv_2.9.1-0.1.jar:$LIBROOT/akka-actor-1.1.3.jar:$LIBROOT/scala-library.jar
 
